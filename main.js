dbb3e36a640671016107d0b68c8640dcc1d6e37b
Gregory Delgado <gregd92694@yahoo.com>
2022-08-16 18:54:22 -0500

    mouse coordinate system add

--- main.js
+++ main.js
@@ -115,3 +115,14 @@ controls.update();
 controls.enablePan = false;
 controls.enableDamping = true;
 
+const mouse= {
+  x:undefined,
+  y: undefined
+}
+
+addEventListener('mousemove',(event)=>{
+
+  mouse.x=(event.clientX/innerWidth)*2 -1
+  mouse.y=-(event.clientY/innerHeight)*2 +1
+  console.log(mouse);
+})
